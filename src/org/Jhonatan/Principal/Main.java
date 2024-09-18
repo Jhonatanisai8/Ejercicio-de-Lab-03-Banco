a6bf5bf942dad615bea221e83f9d0a949cfe5443
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-09-18 10:18:54 -0500

    llamado del formulario principal en la clase main

---
+++ src/org/Jhonatan/Principal/Main.java
@@ -0,0 +1,16 @@
+package org.Jhonatan.Principal;
+
+import org.Jhonatan.Modelo.Cliente;
+import org.Jhonatan.Vista.frmPrincipal;
+
+public class Main {
+    
+    public static Cliente cliente;
+    
+    public static void main(String[] args) {
+        frmPrincipal p = new frmPrincipal();
+        p.setVisible(true);
+       
+    }
+    
+}
